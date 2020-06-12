b9df32289a8f4f9290569290c3c6d70fe2d9e02c
kguertin <kevin.m.guertin@gmail.com>
2020-06-12 15:47:39 -0400

    add hashed passwords to user reg

--- models/user.js
+++ models/user.js
@@ -17,6 +17,13 @@ class User {
     const db = getDb();
     return db.collection('users').findOne({username: username})
   }
+
 }
 
 module.exports = User;
+
+//Things users need 
+// favourited/tagged places however we want to track map pins 
+// some relation to chat, maybe an external library handles this?
+// schedule 
+//
