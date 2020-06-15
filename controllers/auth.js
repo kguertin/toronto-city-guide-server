ca346abe68369cb4e56885887c486a9ae34bfcae
kguertin <kevin.m.guertin@gmail.com>
2020-06-14 22:29:34 -0400

    generate jwt when user logs in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.postLogin = async (req, res) => {
@@ -12,12 +13,19 @@ exports.postLogin = async (req, res) => {
     if (!user) {
       res.status(401).json({msg: 'Please enter a valid username'});
     }
-    
+
     const validPass = await bcrypt.compare(password, user.password);
     if (!validPass) {
       res.status(401).json({msg: "Please enter a valid password"})
     }
 
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+    res.json({token, user: {
+        id: user._id,
+        username: user.username,
+        email: user.email 
+    }});
+    
   } catch (err) {
     res.status(500).json({msg: err.message})
   }
